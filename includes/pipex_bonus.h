68e8e958c7be784ab767fe41ea4d91f2deba5953
Aija Repsa <120313261+AijaRe@users.noreply.github.com>
2023-08-15 15:03:18 +0100

    updated pipex headers

--- includes/pipex_bonus.h
+++ includes/pipex_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arepsa <arepsa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/15 11:32:35 by arepsa            #+#    #+#             */
-/*   Updated: 2023/08/15 12:03:22 by arepsa           ###   ########.fr       */
+/*   Created: 2023/08/15 11:32:28 by arepsa            #+#    #+#             */
+/*   Updated: 2023/08/15 12:03:58 by arepsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
 # include "../libft/libft.h"
 /* perror */
@@ -25,12 +25,15 @@
 /* wait */
 # include <sys/wait.h>
 
-/* pipex.c */
+/* pipex_bonus.c */
 int		main(int argc, char **argv, char **env);
-/* pipex_path.c */
-char	**create_path_array(char **env);
+/* pipex_bonus_path.c */
 char	*find_path(char *cmd, char **path);
-/* pipex_utils.c */
+char	**create_path_array(char **env);
+/* pipex_bonus_heredoc.c */
+void	open_here_doc(char *here_doc);
+void	get_input(char *limiter);
+/* pipex_bonus_utils.c */
 void	check_file_access(char *filename, int flag);
 void	check_input(int argc);
 void	free_tab_and_exit(char **strs, char *message);
